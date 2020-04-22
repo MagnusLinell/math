dbcf74b2d7f7ce5765b3bbdf244edd08b88ee24e
Magnus Linell <magnus.linell@gmail.com>
2020-04-22 20:09:38 +0200

    Add functions

---
+++ functions/read-courses.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const co = require('co');
+
+const uri = 'mongodb+srv://math:3nRNjorTeiqszDBN@cluster0-tvvgo.mongodb.net/test?retryWrites=true&w=majority';
+
+exports.handler = function (event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+  run()
+    .then(res => {
+      callback(null, res);
+    })
+    .catch(error => callback(error));
+};
+
+const run = () => {
+  return co(function* () {
+
+    if (conn == null) {
+      conn = yield mongoose.createConnection(uri, {
+        bufferCommands: false,
+        bufferMaxEntries: 0
+      });
+      conn.model('courses', new mongoose.Schema({
+        name: String,
+        title: String,
+        dependentOn: String,
+        text: Number,
+      }));
+    }
+
+    const Model = conn.model('courses');
+
+    const doc = yield Model.find({});
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(doc)
+    };
+    return response;
+  });
+};

76ee82891ebc400e8de9d2028749027fe803d3c0
Magnus Linell <magnus.linell@gmail.com>
2020-04-22 21:25:50 +0200

    add access control headers

--- functions/edit-course.js
+++ functions/edit-course.js
@@ -48,7 +48,15 @@ const run = ({ name, title, text, dependentOn }) => {
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(saved)
+      body: JSON.stringify(saved),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Max-Age': '2592000',
+        'Access-Control-Allow-Credentials': 'true',
+      }
     };
     console.log({ response });
     return response;
